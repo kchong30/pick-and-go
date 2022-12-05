08785c6a2af17c51697b4509270145dc9e861b2c
Kevin Chong <kchong30@my.bcit.ca>
2022-12-05 11:52:30 -0800

    Created AdminController

---
+++ pick-and-go/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pick_and_go.Models;
+using System.Diagnostics;
+
+namespace pick_and_go.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
