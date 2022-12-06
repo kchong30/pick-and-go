2bcade65ad93c4cad4965108d741415b94ec411d
Byul Kim <caxbelle@gmail.com>
2022-12-05 16:10:14 -0800

    Add pending/completed

--- pick-and-go/Controllers/AdminController.cs
+++ pick-and-go/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using pick_and_go.Data;
+using pick_and_go.ViewModels;
 using pick_and_go.Models;
 using System.Diagnostics;
+using pick_and_go.Repository;
 
 namespace pick_and_go.Controllers
 {
@@ -17,6 +19,17 @@ namespace pick_and_go.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Overview()
+        {
+            OrderHeaderRepository ohRepo = new OrderHeaderRepository(_db);
+            OrderHeaderVM ohVM = new OrderHeaderVM();
+
+            ohVM.Pending = ohRepo.GetAll().Item1;
+            ohVM.Completed = ohRepo.GetAll().Item2;
+
+            return View(ohVM);
         }
     }
 }
