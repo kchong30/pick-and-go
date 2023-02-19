5747fc0bea0ae9084e360ec21309f41d6e73c6e3
Yuko Ishida <yishida2@my.bcit.ca>
2023-02-18 16:36:55 -0800

    accordinon

--- pick-and-go/Controllers/OrderController.cs
+++ pick-and-go/Controllers/OrderController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Mvc;
 using PickAndGo.Models;
 using PickAndGo.Repositories;
 using PickAndGo.ViewModels;
+using System.Net.NetworkInformation;
+using System;
+using Newtonsoft.Json;
+
 
 namespace PickAndGo.Controllers
 {
@@ -33,5 +38,69 @@ namespace PickAndGo.Controllers
 
             return View(vm);
         }
+
+        public IActionResult ShoppingCart()
+        {
+
+
+            // Retrieve JSON data
+                        string jsonData = @"[
+                {
+                    ""productId"": ""1"",
+                    ""description"": ""Small Sandwich"",
+                    ""ingredient"": [
+                        {
+                            ""ingredientId"": 2,
+                            ""description"": ""Wheat Bread"",
+                            ""quantity"": ""1""
+                        },
+                        {
+                            ""ingredientId"": 6,
+                            ""description"": ""Tomato"",
+                            ""quantity"": ""2""
+                        },
+                        {
+                            ""ingredientId"": 8,
+                            ""description"": ""Cucumber"",
+                            ""quantity"": ""1""
+                        }
+                    ],
+                    ""subtotal"": ""13.50""
+                },
+                {
+                    ""productId"": ""1"",
+                    ""description"": ""Large Sandwich"",
+                    ""ingredient"": [
+                        {
+                            ""ingredientId"": 1,
+                            ""description"": ""Italian Bread"",
+                            ""quantity"": ""1""
+                        },
+                        {
+                            ""ingredientId"": 5,
+                            ""description"": ""Ham"",
+                            ""quantity"": ""2""
+                        },
+                        {
+                            ""ingredientId"": 8,
+                            ""description"": ""Cucumber"",
+                            ""quantity"": ""2""
+                        }
+                    ],
+                    ""subtotal"": ""15.20""
+                }
+            ]";
+
+            // Deserialize JSON into C# object
+            
+            List<ShoppingCartVM> items = JsonConvert.DeserializeObject<List<ShoppingCartVM>>(jsonData);
+
+            // Pass C# object to Razor view
+            return View(items);
+
+
+        }
+
+
     }
 }
