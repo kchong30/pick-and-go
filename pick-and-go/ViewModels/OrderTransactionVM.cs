02dcbbf90859c52049e96e1497c38b985804ae8b
lunagrove <sholmes47@my.bcit.ca>
2023-02-25 11:15:13 -0800

    Added transactions display

---
+++ pick-and-go/ViewModels/OrderTransactionVM.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace PickAndGo.ViewModels
+{
+    public class OrderTransactionVM
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string OrderDate { get; set; }
+        public decimal OrderValue { get; set; }
+        public string? Currency { get; set; }
+        public string? PaymentType { get; set; }
+        public string? PaymentId { get; set; }
+        public string PaymentDate { get; set; }
+    }
+}
